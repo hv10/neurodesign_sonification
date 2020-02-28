3b6d2d54df9f8cda539b9c4da9d4ef86da2b33cd
hv10 <noeldanz@gmail.com>
2020-02-28 17:00:22 +0100

    add static site update

---
+++ docs/precache-manifest.10f7e0d182e288ad6356193325770d58.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d1ccdd4bbd062108239b25062b7f02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0de9f8c0e30d9e522a4",
+    "url": "/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "c64a0bb302f442aae7e2",
+    "url": "/static/css/main.42222c07.chunk.css"
+  },
+  {
+    "revision": "a0de9f8c0e30d9e522a4",
+    "url": "/static/js/2.86b71e31.chunk.js"
+  },
+  {
+    "revision": "7341e16c628d12f401e488dad01e9978",
+    "url": "/static/js/2.86b71e31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c64a0bb302f442aae7e2",
+    "url": "/static/js/main.c34c7501.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  },
+  {
+    "revision": "3b0eb74b3d010e0a69a5c47698e0c89b",
+    "url": "/static/media/brain-overview.3b0eb74b.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
